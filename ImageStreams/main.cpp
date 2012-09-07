faf9cf1aa11af9c63b203d0284bede2fdc879f95
Joshua Moerman <lakseru@gmail.com>
2012-09-07 19:33:35 +0200

    Initial Commit

---
+++ ImageStreams/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ImageStreams
+//
+//  Created by Joshua Moerman on 9/7/12.
+//  Copyright (c) 2012 Vadovas. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
